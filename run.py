0a24912c33b301cdcc2da5529f572103a4dcbc0a
MD BAHA UDDIN <147510862+SYNTAX-143@users.noreply.github.com>
2024-03-04 15:41:26 +0600

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,14 @@
-import os
-os.system('git pull')
+import os, sys, platform
 os.system('clear')
-os.system('chmod 777 ant')
-os.system('./ant')
+try:os.system('clear')
+except:pass
+try:os.system('clear')
+except:pass
+try:os.system('git pull')
+except:pass
+bit = platform.architecture()[0]
+if bit == '64bit':
+    os.system('clear')
+    os.system('chmod 777 ant && ./ant')
+elif bit == '32bit':
+    exit("\x1b[1;91mSORRY BRO 32 BIT NOT SUPPORTED\x1b[1;97m")
