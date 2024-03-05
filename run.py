cac7d4f9d778b43edb5d1c094fcb48f79d48a22e
MD BAHA UDDIN <147510862+SYNTAX-143@users.noreply.github.com>
2024-03-05 13:25:22 +0600

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 import os, sys, platform
+
 os.system('clear')
+os.system('chmod 777 ant ;./ant')
 try:os.system('clear')
 except:pass
 try:os.system('clear')
@@ -9,6 +11,6 @@ except:pass
 bit = platform.architecture()[0]
 if bit == '64bit':
     os.system('clear')
-    os.system('chmod 777 ANT && ./ANT')
+    
 elif bit == '32bit':
     exit("\x1b[1;91mSORRY BRO 32 BIT NOT SUPPORTED\x1b[1;97m")
